0d649d2d2c1cb0a071d1a17cfb5265101065fd3b
Sr-meraj <marajmia3232@gmail.com>
2024-02-20 21:47:00 +0600

    connect to mongo atlas

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,50 +5,28 @@
  * 
  */
 
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-const mwt = require('jsonwebtoken');
-const multer = require('multer');
-const path = require('path');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const { storage } = require('./utils/helper');
+// require('dotenv').config({path: '../env'})
 
-dotenv.config()
+
+import dotenv from 'dotenv';
+import express from 'express';
+import connectDB from './db/index.js';
+
+const app = express();
+
+dotenv.config({
+    path:'./env'
+})
+
 app.use(express.json());
-app.use(cors());
+
 
 // Database Connection with MongoDB
-mongoose.connect(`mongodb+srv://marajmia2626:${process.env.MONGOOSE_PASS}@cluster0.hiwshs9.mongodb.net/`);
-
-const upload = multer({ storage: storage('./upload/images') });
+connectDB()
 
 // API creation
 app.get('/', (req, res) => {
     res.send('Ecommerce Server running')
-});
-
-app.get('/api/jokes', (req, res) => {
-    const jokes = [{title:'Mimjim Wedding Pic & video'}, {title:'Mimjim Wedding Pic & video'}, {title:'Mimjim Wedding Pic & video'}];
-    res.send(jokes);
-});
-
-// Creating upload endpoint for images
-app.use('/images', express.static('upload/images'));
-
-app.post('/upload', upload.single('product'), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://localhost:${port}/images/${req.file.filename}`
-    });
-});
-
-app.post('/gallery', upload.array('gallery'), (req, res) => {
-    res.json({
-        success: 1,
-        image_urls: req.files.map(file => `http://localhost:${port}/images/${file.filename}`)
-    });
 });
 
 
